7e122e590400d2a054b54e6079502e7872bcb159
AlisonYoon <politescarlett1@gmail.com>
2019-10-29 21:33:24 +0000

    add user form added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import UserTable from './table/UserTable';
+import AddUserForm from './forms/AddUserForm';
 
 const App = () => {
     const usersData = [
@@ -10,12 +11,19 @@ const App = () => {
     ]
 
     const [users, setUsers] = useState(usersData)
+
+    const addUser = user => {
+        user.id = users.length + 1
+        setUsers([...users, user])
+    }
+
   return (
       <div className="container">
           <h1>CRUD App with Hooks</h1>
           <div className="flex-row">
               <div className="flex-large">
                   <h2>Add user</h2>
+                  <AddUserForm addUser={addUser}/>
               </div>
               <div className="flex-large">
                   <h2>View users</h2>
